7ee583733cbd15e8fea38be19098012688f3c480
takenokonyokki <97602717+takenokonyokki@users.noreply.github.com>
2022-02-26 14:17:48 +0000

    [add]商品新規登録画面70％および商品詳細画面30％追加

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -7,7 +7,6 @@ class Admin::GenresController < ApplicationController
   def create
     @genre = Genre.new(genre_params)
     @genre.save
-    @genres = Genre.all
     redirect_to admin_genres_path(@genre.id)
   end
 
--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -6,9 +6,22 @@ class Admin::ItemsController < ApplicationController
     @item = Item.new
   end
 
+  def create
+    @item = Item.new(params[:id])
+    @item.save
+    redirect_to admin_item_path(@item.id)
+  end
+
   def show
+    @item = Item.find(params[:id])
   end
 
   def edit
   end
+
+  private
+  def item_params
+    params.require(:item).permit(:genre_id, :name, :introduction, :price, :is_active, :image)
+  end
+
 end
