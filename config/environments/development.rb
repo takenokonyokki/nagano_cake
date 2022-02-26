346340fa9fd860d14cf0f42bff3b75e5cc7ae03b
takenokonyokki <97602717+takenokonyokki@users.noreply.github.com>
2022-02-26 06:39:32 +0000

    [add]開発準備完了

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,7 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+  config.hosts << "e298dbb9394a4ef48a2e874212102be9.vfs.cloud9.us-east-1.amazonaws.com"
+
 end
