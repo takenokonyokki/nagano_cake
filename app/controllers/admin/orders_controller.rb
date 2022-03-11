609263dc2581d2b8b343d2b715705bcbd892332d
takenokonyokki <97602717+takenokonyokki@users.noreply.github.com>
2022-03-11 14:01:21 +0000

    [add]再度テスト実施後バグ修正

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -7,7 +7,7 @@ class Admin::OrdersController < ApplicationController
     @order = Order.find(params[:id])
     @order_details = @order.order_details
     @order.update(order_params)
-    if @order.status = "payment_confirmation"
+    if @order.status == "payment_confirmation"
       @order.order_details.each do |order_detail|
         order_detail.making_status = "production_pending"
         order_detail.save
